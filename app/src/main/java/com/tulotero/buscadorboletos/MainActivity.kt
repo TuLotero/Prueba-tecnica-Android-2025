91ead032996b789522841507ab06d5794aa577b2
Óscar Delgado Santamaría <oscar@tulotero.es>
2025-01-26 11:51:52 +0100

    Añadido Activity vacío básico

---
+++ app/src/main/java/com/tulotero/buscadorboletos/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.tulotero.buscadorboletos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
